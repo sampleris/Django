89a141cdbf857bd94df7426da945aafeceb96124
sampleris <mpara.romaric@gmail.com>
2024-06-01 08:18:11 +0100

    code

--- blog/views.py
+++ blog/views.py
@@ -11,13 +11,13 @@ posts = [
 },
 {
  
-        'author': ' Mpara Carine',
+        'author': 'Carine',
         'title': 'Blog post2',
         'content': 'second post content',
         'date_posted': 'February 28, 2023'   
 },
 {
-    'author': ' Mpara Faith',
+    'author': 'Faith',
         'title': 'Blog post3',
         'content': 'Third post content',
         'date_posted': 'February 28, 2023'   
@@ -35,5 +35,8 @@ def about(request):
     return render(request, 'blog/about.html', {'title': 'About Page'})
 def bootstrap(request):
     return render(request, 'blog/' )
+def register(request):
+    return render(request, 'users/register.html')
+
 
 # Create your views here.
